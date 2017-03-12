71301b8c9d7d552e40b4105930a7b7ae670825c3
itsankoff <ivaylotsankov93@gmail.com>
2017-03-12 21:06:17 +0200

    * Add exec mode for pipelines program

--- pipelines/main.go
+++ pipelines/main.go
@@ -2,16 +2,41 @@ package main
 
 import (
 	"fmt"
+	"github.com/itsankoff/go-play/pipelines/parallel"
 	"github.com/itsankoff/go-play/pipelines/serial"
+	"os"
 )
 
 func main() {
-	files, err := serial.Hasher(".")
-	if err != nil {
-		fmt.Println(err)
+	if len(os.Args) < 2 {
+		fmt.Println("usage: pipelines <mode (serial|parallel)> <directory>")
+		os.Exit(1)
 	}
 
-	for path, digest := range files {
-		fmt.Printf("%x, %s\n", digest, path)
+	mode := os.Args[1]
+	dir := os.Args[2]
+	fmt.Println("Mode: ", mode)
+	if mode == "serial" {
+		// 		files, err := serial.Hasher(dir)
+		// 		if err != nil {
+		// 			fmt.Println(err)
+		// 		}
+		//
+		// 		for path, digest := range files {
+		// 			fmt.Printf("%x, %s\n", digest, path)
+		// 		}
+		serial.Hasher(dir)
+	}
+
+	if mode == "parallel" {
+		// 		files, err := parallel.Hasher(dir)
+		// 		if err != nil {
+		// 			fmt.Println(err)
+		// 		}
+		//
+		// 		for path, digest := range files {
+		// 			fmt.Printf("%x, %s\n", digest, path)
+		// 		}
+		parallel.Hasher(dir)
 	}
 }
